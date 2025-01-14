84786ad9afabf0f110a15eb26e219be7f1e3a548
LaLoupiotte <gui260903@gmail.com>
2025-01-14 14:13:04 +0100

    Added setup.py and main.py

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ASCII_art-guilherme_miranda_martins-5
 string -> ascii art
+
+
---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='{ASCII_art}',
+    version='1.0',
+    author='{LaLoupiotte}',
+    license='MIT',
+    long_description=open('README.md').read(),
+)
